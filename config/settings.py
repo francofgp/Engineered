7efe5ced77bc6893a78865b1bc021d5735e5237d
unknown <giulianopertile@gmail.com>
2022-05-15 23:24:07 -0300

    Add enviromental variables

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from tkinter import ALL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i-&k_-8*o_3&xv+i*2uhad*i26tj!!kc)0^+5!r+xe#e3e-=7o'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", 'django-insecure-i-&k_-8*o_3&xv+i*2uhad*i26tj!!kc)0^+5!r+xe#e3e-=7o')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == '1'  # 1 == True
 
 ALLOWED_HOSTS = []
-
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get("ALLOWED_HOST")]
+    print(ALLOWED_HOSTS)
 
 # Application definition
 
